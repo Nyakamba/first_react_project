4fa8d22420c05572c4c96f7e81711abbe59bfd59
Nyakamba <omwegaenock@gmail.com>
2023-04-12 17:33:54 +0300

    header.js

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -18,12 +18,13 @@ const Header = () => {
           <img src= {people} alt='people'/>
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className='gpt3_header-image'>
-          <img src= {ai} alt= 'ai'/>
-        </div>
+      </div>
+      <div className='gpt3__header-image'>
+        <img src= {ai} alt= 'ai'/>
       </div>
     </div>
   )
 }
+
 
 export default Header
