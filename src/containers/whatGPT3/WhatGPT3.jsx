10434319efb579dd1e346758e87e7a8723e6e11f
Nyakamba <omwegaenock@gmail.com>
2023-04-13 22:00:47 +0300

    whatgpt3.jsx

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -9,6 +9,15 @@ const WhatGPT3 = () => {
       <div className='gpt3__whatgpt3-feautre'>
         <Feature />
       </div>
+      <div className='gpt3__whatgpt3-header'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+          <Feature />
+          <Feature />
+          <Feature />
+      </div>
     </div>
   )
 }
