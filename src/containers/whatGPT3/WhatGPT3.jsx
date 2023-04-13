90fe8b81502b9778f44443bb94d90b7fa8b456e6
Nyakamba <omwegaenock@gmail.com>
2023-04-13 18:56:47 +0300

    Whatgpt3.jsx

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './whatGPT3.css';
+import {Feature} from '../../components/index.jsx'
+
+
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
-      <div className='gpt3__whatgpt3-feature'>
+    <div className='gpt3__whatgpt3' id='whpt3'>
+      <div className='gpt3__whatgpt3-feautre'>
         <Feature />
-      </div>    
+      </div>
     </div>
   )
 }
