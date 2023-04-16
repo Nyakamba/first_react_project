3430a239e8fb6cb1023566f368c2b2d324153639
Nyakamba <omwegaenock@gmail.com>
2023-04-16 23:59:18 +0300

    footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './footer.css';
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='gpt3__footer section__padding'>
+      <div gpt3__footer-heading>
+        <h1 className='gradient__text'></h1>
+      </div>
     </div>
   )
 }
