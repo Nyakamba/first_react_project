817b1099615f94dacd64d5189367bd57c1675faf
Nyakamba <omwegaenock@gmail.com>
2023-04-16 20:17:54 +0300

    blog.jsx

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './blog.css';
 import {Article} from "../../components"
-import {blog01, blog02, blog03, blog04, blog05} from './'
+import {blog01, blog02, blog03, blog04, blog05} from './imports'
 
 
 const Blog = () => {
